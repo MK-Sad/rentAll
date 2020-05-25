eb3a93be39bbf03d93f2b4a71a2040d6eeea81a2
MonikaSadłowska <monikasadlowska@postgraduate.put.poznan.pl>
2020-05-25 19:17:31 +0200

    more tests

---
+++ src/test/java/com/monika/rentaladder/User/RentalEventHandlerTest.java
@@ -0,0 +1,62 @@
+package com.monika.rentaladder.User;
+
+import com.monika.rentaladder.User.MailSender.MailService;
+import com.monika.rentaladder.User.MailSender.RentalEvent;
+import com.monika.rentaladder.User.MailSender.RentalEventHandler;
+import com.monika.rentaladder.User.UserDTOs.UserEntity;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
+class RentalEventHandlerTest {
+
+    @Spy
+    UserFacade userFacade = new UserConfiguration().userFacade();
+
+    @Mock
+    MailService mockMailService;
+
+    @InjectMocks
+    RentalEventHandler rentalEventHandler;
+
+    @Captor
+    ArgumentCaptor<UserEntity> captorUser;
+    @Captor
+    ArgumentCaptor<UserEntity> captorOwner;
+    @Captor
+    ArgumentCaptor<String> captorName;
+    @Captor
+    ArgumentCaptor<Long> captorId;
+
+    @Test
+    void addingPointsOnRentalTest() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        //given
+        UserEntity user = new UserEntity();
+        user.setName("Monika");
+        user.setPoints(0);
+        UserEntity owner = new UserEntity();
+        owner.setName("Kazik");
+        owner.setPoints(0);
+        userFacade.addUser(user);
+        userFacade.addUser(owner);
+        RentalEvent rentalEvent = new RentalEvent(RentalEvent.Type.CONTACT, user.getName(),
+                owner.getName(), "item name",  2L);
+
+        //when
+        rentalEventHandler.eventListener(rentalEvent);
+
+        //then
+        user = userFacade.getUserByName(owner.getName());
+        assertEquals(+2, user.getPoints());
+        //ArgumentCaptor<Person> argument = ArgumentCaptor.forClass(Person.class);
+        verify(mockMailService).sendOwnerDataMail(captorUser.capture(), captorOwner.capture(),
+                captorName.capture(), captorId.capture());
+        assertEquals("Monika", captorUser.getValue().getName());
+        assertEquals("Kazik", captorOwner.getValue().getName());
+        assertEquals("item name", captorName.getValue());
+        assertEquals(2L, captorId.getValue());
+    }
+}
